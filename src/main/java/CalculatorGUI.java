692b034a2b4b9f5f330bffa395a897b0573ce534
mohbes <mohbes@IMM.INTRA>
2016-11-02 11:29:25 +0100

    Massomeh commit 2: the design of the calculator, also adding action performed to the new buttons has been finished.

--- src/main/java/CalculatorGUI.java
+++ src/main/java/CalculatorGUI.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.Font;
 
 public class CalculatorGUI implements ActionListener{
 	private JFrame frmCalculator;
@@ -21,6 +22,23 @@ public class CalculatorGUI implements ActionListener{
 	private JButton btnSubtract = new JButton("-");
 	private JButton btnMultiply = new JButton("*");
 	private JButton btnDivide = new JButton("/");
+	private final JButton btnPow = new JButton("POW");
+	private final JButton btnSqr = new JButton("SQR");
+	private final JButton btnMod = new JButton("MOD");
+	private final JButton btnLog = new JButton("log");
+	private final JButton button_0 = new JButton("0");
+	private final JButton button_1 = new JButton("1");
+	private final JButton button = new JButton("2");
+	private final JButton button_2 = new JButton("3");
+	private final JButton button_3 = new JButton("4");
+	private final JButton button_4 = new JButton("5");
+	private final JButton button_5 = new JButton("6");
+	private final JButton button_6 = new JButton("7");
+	private final JButton button_7 = new JButton("8");
+	private final JButton button_8 = new JButton("9");
+	private final JButton btnEqual = new JButton("=");
+	private final JButton btnSin = new JButton("Sin");
+	private final JButton btnRnd = new JButton("rnd");
 
 	
 	/**
@@ -39,7 +57,7 @@ public class CalculatorGUI implements ActionListener{
 		frmCalculator = new JFrame();
 		frmCalculator.getContentPane().setBackground(Color.CYAN);
 		frmCalculator.setTitle("Calculator");
-		frmCalculator.setBounds(100, 100, 255, 300);
+		frmCalculator.setBounds(100, 100, 704, 422);
 		frmCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmCalculator.getContentPane().setLayout(null);
 		
@@ -55,12 +73,12 @@ public class CalculatorGUI implements ActionListener{
 		
 		lblFirstNumber.setBounds(10, 30, 96, 14);
 		lblSecondNumber.setBounds(10, 61, 96, 14);
-		lblResult.setBounds(10, 86, 223, 14);
+		lblResult.setBounds(20, 96, 223, 14);
 
-		btnAdd.setBounds(10, 139, 47, 23);
-		btnSubtract.setBounds(67, 139, 48, 23);	
-		btnMultiply.setBounds(125, 139, 47, 23);	
-		btnDivide.setBounds(182, 139, 37, 23);	
+		btnAdd.setBounds(40, 183, 40, 25);
+		btnSubtract.setBounds(99, 183, 40, 25);	
+		btnMultiply.setBounds(151, 183, 40, 25);	
+		btnDivide.setBounds(203, 183, 40, 25);	
 		
 		frmCalculator.setVisible(true);
 	}
@@ -78,6 +96,99 @@ public class CalculatorGUI implements ActionListener{
 		frmCalculator.getContentPane().add(btnSubtract);
 		frmCalculator.getContentPane().add(btnMultiply);
 		frmCalculator.getContentPane().add(btnDivide);
+		btnPow.setFont(new Font("Symbol MT", Font.PLAIN, 8));
+		btnPow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSqr.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSqr.setBounds(40, 145, 40, 25);
+		frmCalculator.getContentPane().add(btnSqr);
+		
+		btnPow.setBounds(90, 145, 40, 25);
+		frmCalculator.getContentPane().add(btnPow);
+		btnMod.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+	
+		
+		btnMod.setBounds(140, 145, 40, 25);		
+		frmCalculator.getContentPane().add(btnMod);
+		btnLog.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		btnLog.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnLog.setBounds(262, 145, 40, 25);
+		frmCalculator.getContentPane().add(btnLog);
+		
+		button_0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_0.setBounds(40, 339, 40, 25);
+		
+		frmCalculator.getContentPane().add(button_0);
+		button_1.setBounds(40, 301,40, 25);
+		
+		frmCalculator.getContentPane().add(button_1);
+		button.setBounds(120, 301, 40, 25);
+		
+		frmCalculator.getContentPane().add(button);
+		button_2.setBounds(203, 301, 40, 25);
+		
+		frmCalculator.getContentPane().add(button_2);
+		button_3.setBounds(40, 263, 40, 25);
+		
+		frmCalculator.getContentPane().add(button_3);
+		button_4.setBounds(120, 263, 40, 25);
+		
+		frmCalculator.getContentPane().add(button_4);
+		button_5.setBounds(203, 263, 40, 25);
+		
+		frmCalculator.getContentPane().add(button_5);
+		button_6.setBounds(40, 223, 40, 25);
+		
+		frmCalculator.getContentPane().add(button_6);
+		button_7.setBounds(120, 223, 40, 25);
+		
+		frmCalculator.getContentPane().add(button_7);
+		button_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_8.setBounds(203, 223, 40, 25);
+		
+		frmCalculator.getContentPane().add(button_8);
+		btnEqual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEqual.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnEqual.setBounds(262, 221, 40, 143);
+		
+		frmCalculator.getContentPane().add(btnEqual);
+		btnSin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSin.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnSin.setBounds(262, 185, 40, 25);
+		
+		frmCalculator.getContentPane().add(btnSin);
+		btnRnd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRnd.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		btnRnd.setBounds(203, 145, 40, 25);
+		
+		frmCalculator.getContentPane().add(btnRnd);
 	}
 		
 	
