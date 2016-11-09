06c5250f0c6ff5b0e99460dd02a238992a9fff1f
Mojgan Akhavan <Mojgan@Mojgans-MacBook-Pro.local>
2016-11-09 21:45:14 +0100

    mojgan 14th commit:Jacoco and surefire report generated Conflict resolved;)

--- src/main/java/BasicCalculator.java
+++ src/main/java/BasicCalculator.java
@@ -18,17 +18,12 @@ public class BasicCalculator implements CalculatorBasicOperationsInterface  {
 		
 		public double divide(double firstNumber, double secondNUmber){
 			double result=0.0;
-			//try{
-				
+			
 				result = firstNumber / secondNUmber;
 			
-			/*} catch (ArithmeticException e) { // 
-				  System.out.println("Division by zero." +e);
-				  return -1111.1111;
-				}*/
+			
 			
 			if (Double.isInfinite(result)){
-			//JOptionPane.showMessageDialog(null, "division by zero!", "Error message", JOptionPane.ERROR_MESSAGE);			
 				return -1111.1111;
 			}		
 			
